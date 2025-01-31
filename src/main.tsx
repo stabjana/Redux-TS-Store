0cc42b1588bc7b69d68ab58945193de7d0de2fac
Jana Stefanie <s2400789@BC-CWRW4DVTW1-Kannettava-tietokone.local>
2025-01-31 11:55:55 +0200

    add provider to main

--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,13 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import { Provider } from 'react-redux';
+import { store } from './store/store.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Provider store={store}>
     <App />
+    </ Provider>
   </StrictMode>,
 )
